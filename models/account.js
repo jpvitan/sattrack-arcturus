735a82c07da07bbf3105793afe85e948c112792e
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-10-27 03:47:33 +0800

    Create files for accounts

---
+++ models/account.js
@@ -0,0 +1,10 @@
+/*
+
+sattrack-node
+RESTful API for retrieving useful satellite information.
+
+LICENSE: MIT License
+Created by Justine Paul Sanchez Vitan.
+Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
+
+*/
