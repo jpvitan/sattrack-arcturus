a00f6783cb63ff505f749981d247e42d60622a91
Justine Paul Sanchez Vitan <justinepaulvitan5@gmail.com>
2022-11-19 22:46:04 +0800

    Add admin check middleware

--- routes/auth.js
+++ routes/auth.js
@@ -13,3 +13,8 @@ module.exports.checkAuthentication = async (req, res, next) => {
   if (req.isAuthenticated()) return next()
   return res.status(401).json({ message: 'Not Authenticated' })
 }
+
+module.exports.checkAdministrator = async (req, res, next) => {
+  if (req.user.type === 'admin') return next()
+  return res.status(403).json({ message: 'Authorization Error' })
+}
