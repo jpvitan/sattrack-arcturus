e88a6bd566ab6853e786cd304fba05863d61e5ba
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-04-18 03:53:56 +0800

    Update source code notice

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,11 +1,15 @@
 /*
 
-sattrack-node
-RESTful API for retrieving useful satellite information.
+SatTrack-Arcturus
+A RESTful API built with Node.js and Express that lets you retrieve useful satellite information by providing identifiers assigned by the North American Aerospace Defense Command.
 
-LICENSE: MIT License
-Created by Justine Paul Sanchez Vitan.
-Copyright © 2022 Justine Paul Sanchez Vitan. All rights reserved.
+This project is under the MIT license.
+Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
+
+Copyright © 2022 Justine Paul Vitan. All rights reserved.
+
+License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LICENSE
+Developer's Website: https://jpvitan.com/
 
 */
 
