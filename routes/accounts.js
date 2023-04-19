1c6d40188e1fac42eeae40e8b3e6eec22429e359
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-04-20 02:27:03 +0800

    Add route for sign-in and sign-out

--- routes/accounts.js
+++ routes/accounts.js
@@ -14,6 +14,7 @@ Developer's Website: https://jpvitan.com/
 */
 
 const express = require('express')
+const passport = require('passport')
 const auth = require('../middlewares/auth')
 const Account = require('../models/account')
 const bcrypt = require('bcrypt')
@@ -58,6 +59,17 @@ router.post('/', async (req, res) => {
   } catch (error) {
     return res.status(500).json({ message: 'Internal Server Error' })
   }
+})
+
+router.post('/signin', passport.authenticate('local'), async (req, res) => {
+  return res.status(200).json({ message: 'Account Verified' })
+})
+
+router.post('/signout', auth.checkAuthentication, async (req, res) => {
+  req.logout((error) => {
+    if (error) return res.status(500).json({ message: 'Internal Server Error' })
+    return res.status(200).json({ message: 'Account Signed Out' })
+  })
 })
 
 module.exports = router
