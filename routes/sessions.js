3f005b457232f271b9e116bd44a78a9c5647a5b9
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-08-31 19:02:04 +0800

    Handle log in and log out route

--- routes/sessions.js
+++ routes/sessions.js
@@ -12,3 +12,21 @@ License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LI
 Developer's Website: https://jpvitan.com/
 
 */
+
+const express = require('express')
+const passport = require('passport')
+
+const router = express.Router()
+
+router.post('', passport.authenticate('local'), async (req, res) => {
+  return res.status(200).json({ message: 'Account Verified' })
+})
+
+router.delete('', async (req, res) => {
+  req.logout((error) => {
+    if (error) return res.status(500).json({ message: 'Internal Server Error' })
+    return res.status(200).json({ message: 'Account Signed Out' })
+  })
+})
+
+module.exports = router
