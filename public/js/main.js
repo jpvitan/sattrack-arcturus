8b7407937e18c4a39ba4bed4ff1b11c36162f681
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-09-22 05:35:18 +0800

    Move services logic

--- public/js/main.js
+++ public/js/main.js
@@ -13,11 +13,24 @@ Developer's Website: https://jpvitan.com/
 
 */
 
+import Account from './services/accounts.js'
+import Session from './services/sessions.js'
+
 document.addEventListener('DOMContentLoaded', () => {
+  setupHome()
   setupSignIn()
   setupSignUp()
-  setupHome()
 })
+
+const setupHome = () => {
+  const signIn = document.getElementById('sign-in')
+  const signInButton = document.getElementById('sign-in-button')
+  const signUp = document.getElementById('sign-up')
+  const signUpButton = document.getElementById('sign-up-button')
+
+  signInButton.onclick = () => { signIn.classList.remove('d-none') }
+  signUpButton.onclick = () => { signUp.classList.remove('d-none') }
+}
 
 const setupSignIn = () => {
   const signIn = document.getElementById('sign-in')
@@ -27,38 +40,18 @@ const setupSignIn = () => {
   const signInUsernameForm = document.getElementById('sign-in-username-form')
   const signInPasswordForm = document.getElementById('sign-in-password-form')
 
-  signInCloseButton.onclick = () => {
-    signIn.classList.add('d-none')
-  }
+  signInCloseButton.onclick = () => { signIn.classList.add('d-none') }
   signInForm.onsubmit = async (e) => {
     e.preventDefault()
 
     const username = signInUsernameForm.value
     const password = signInPasswordForm.value
+    const output = await Session.login({ username, password })
 
-    if (!username || !password) {
-      signInNotice.innerText = 'Please enter a valid username and password.'
-      return
-    }
-
-    try {
-      const response = await fetch(
-        '/sessions',
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ username, password })
-        }
-      )
-      if (response.ok) {
-
-      } else if (response.status === 401) {
-        signInNotice.innerText = 'The username and password you entered does not correspond to any existing account.'
-      } else {
-        signInNotice.innerText = 'The system encountered some unexpected errors. Please try again later.'
-      }
-    } catch (error) {
-      signInNotice.innerText = 'The system encountered some unexpected errors. Please try again later.'
+    if (output.success) {
+      console.log('Success')
+    } else {
+      console.log('Fail')
     }
   }
 }
@@ -78,56 +71,19 @@ const setupSignUp = () => {
   ]
   field.forEach(field => { field.reference = document.getElementById(field.id) })
 
-  signUpCloseButton.onclick = () => {
-    signUp.classList.add('d-none')
-  }
+  signUpCloseButton.onclick = () => { signUp.classList.add('d-none') }
   signUpForm.onsubmit = async (e) => {
     e.preventDefault()
 
-    const body = {}
-    let empty = false
+    const data = {}
+    field.forEach(({ key, reference }) => { data[key] = reference.value })
 
-    field.forEach(({ key, reference }) => {
-      body[key] = reference.value
-      empty = !body[key] ? true : empty
-    })
+    const output = await Account.create(data)
 
-    if (empty) {
-      signUpNotice.classList.add('text-color-red')
-      signUpNotice.innerHTML = 'You are required to fill out all fields!'
-      return
+    if (output.success) {
+      console.log('Success')
+    } else {
+      console.log('Fail')
     }
-    if (body.password !== body.repeat) {
-      signUpNotice.classList.add('text-color-red')
-      signUpNotice.innerHTML = 'Your passwords do not match. Please try again.'
-      return
-    }
-
-    try {
-      const response = await fetch(
-        '/api/accounts',
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body)
-        }
-      )
-    } catch (error) {
-
-    }
-  }
-}
-
-const setupHome = () => {
-  const signIn = document.getElementById('sign-in')
-  const signInButton = document.getElementById('sign-in-button')
-  const signUp = document.getElementById('sign-up')
-  const signUpButton = document.getElementById('sign-up-button')
-
-  signInButton.onclick = () => {
-    signIn.classList.remove('d-none')
-  }
-  signUpButton.onclick = () => {
-    signUp.classList.remove('d-none')
   }
 }
