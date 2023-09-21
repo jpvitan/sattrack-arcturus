485ba3f5d6b59409ab1c235ca92aa477313cd1a8
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-09-21 23:38:50 +0800

    Create sessions module

---
+++ public/js/services/sessions.js
@@ -0,0 +1,54 @@
+/*
+
+SatTrack-Arcturus
+A RESTful API built with Node.js and Express that lets you retrieve useful satellite information by providing identifiers assigned by the North American Aerospace Defense Command.
+
+This project is under the MIT license.
+Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
+
+Copyright © 2022 Justine Paul Vitan. All rights reserved.
+
+License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LICENSE
+Developer's Website: https://jpvitan.com/
+
+*/
+
+export default class Session {
+  static async login (body) {
+    const output = {
+      response: null,
+      message: null,
+      success: true
+    }
+
+    try {
+      output.response = await fetch(
+        '/sessions',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body
+        }
+      )
+    } catch (error) {
+      output.message = 'The system encountered some unexpected errors. Please try again later.'
+      output.success = false
+      return output
+    }
+
+    switch (output.response.status) {
+      case 200:
+        output.message = ''
+        break
+      case 401:
+        output.message = 'The username and password you entered does not correspond to any existing account.'
+        output.success = false
+        break
+      default:
+        output.message = 'The system encountered some unexpected errors. Please try again later.'
+        output.success = false
+    }
+
+    return output
+  }
+}
