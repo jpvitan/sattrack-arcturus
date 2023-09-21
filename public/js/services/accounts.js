e03b8d8e2ee5072fee071023c1d85e909b242f09
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2023-09-22 04:42:29 +0800

    Create accounts module

---
+++ public/js/services/accounts.js
@@ -0,0 +1,20 @@
+/*
+
+SatTrack-Arcturus
+A RESTful API built with Node.js and Express that lets you retrieve useful satellite information by providing identifiers assigned by the North American Aerospace Defense Command.
+
+This project is under the MIT license.
+Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
+
+Copyright © 2022 Justine Paul Vitan. All rights reserved.
+
+License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LICENSE
+Developer's Website: https://jpvitan.com/
+
+*/
+
+export default class Account {
+    static async create () {
+
+    }
+}
