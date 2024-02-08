921dc6b2cd8eb382e02fea81cade3bf26d86ebf0
Justine Paul Vitan <justinepaulvitan5@gmail.com>
2024-02-09 02:20:07 +0800

    Separate key routes

---
+++ routes/api/accounts/keys.js
@@ -0,0 +1,61 @@
+/*
+
+SatTrack-Arcturus
+A RESTful API built with Node.js and Express that lets you retrieve useful satellite information by providing identifiers assigned by the North American Aerospace Defense Command.
+
+This project is under the MIT license.
+Please read the terms and conditions stated within the license before attempting any modification or distribution of the software.
+
+Copyright © 2022 Justine Paul Vitan. All rights reserved.
+
+License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LICENSE
+Developer's Website: https://jpvitan.com/
+
+*/
+
+const crypto = require('crypto')
+const express = require('express')
+const bcrypt = require('bcrypt')
+
+const router = express.Router({ mergeParams: true })
+
+const { verifyAuthentication, verifyAuthorization } = require('../../../middlewares/auth')
+const { getAccount, getKey } = require('../../../middlewares/accounts')
+
+router.get('/', verifyAuthentication(), verifyAuthorization({ allowed: ['admin', 'user'] }), getAccount, async (req, res) => {
+  return res.status(200).json(res.account.keys)
+})
+
+router.post('/', verifyAuthentication(), verifyAuthorization({ allowed: ['admin', 'user'] }), getAccount, async (req, res) => {
+  const { name } = req.body
+
+  const key = crypto.randomUUID()
+
+  try {
+    const hashedKey = await bcrypt.hash(key, 10)
+
+    res.account.keys.push({ name, key: hashedKey })
+    res.account.save()
+
+    return res.status(201).json({ message: 'Key Created', key })
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+})
+
+router.get('/:id', verifyAuthentication(), verifyAuthorization({ allowed: ['admin', 'user'] }), getAccount, getKey, async (req, res) => {
+  return res.status(200).json(res.key)
+})
+
+router.delete('/:id', verifyAuthentication(), verifyAuthorization({ allowed: ['admin', 'user'] }), getAccount, getKey, async (req, res) => {
+  try {
+    res.account.keys.pull(res.key._id)
+    res.account.save()
+
+    return res.status(200).json({ message: 'Key Deleted' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+})
+
+module.exports = router
