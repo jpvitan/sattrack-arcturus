9d3a56a3a489f50f0af2f59f1722e1a247827b2f
Justine Paul Vitan <jpvitanx@gmail.com>
2024-08-19 00:49:10 +0800

    Add swagger options

--- config/swagger.js
+++ config/swagger.js
@@ -12,3 +12,17 @@ License Information: https://github.com/jpvitan/sattrack-arcturus/blob/master/LI
 Developer's Website: https://jpvitan.com/
 
 */
+
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUIExpress = require('swagger-ui-express')
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SatTrack-Arcturus',
+      version: '0.1.0',
+      description: 'A RESTful API built with Node.js and Express that lets you track and predict the orbit of artificial satellites through the use of the Simplified General Perturbations-4 (SGP4) model.'
+    }
+  }
+}
